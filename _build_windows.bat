0afe547aefff06eb5ba1b4359d99275a79f73a87
Dave De Breuck <dave.debreuck@twikit.com>
2022-03-03 15:18:31 +0100

    Windows scripts

---
+++ _build_windows.bat
@@ -0,0 +1,1 @@
+Powershell.exe -executionpolicy remotesigned -File .\scripts\windows\build.ps1 %*
---
+++ _generate_windows.bat
@@ -0,0 +1,1 @@
+Powershell.exe -executionpolicy remotesigned -File .\scripts\windows\generate.ps1 %*
