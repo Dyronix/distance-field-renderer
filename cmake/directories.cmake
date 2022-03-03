c03158325cdbe0d711b765e6c5b16a9a957f1746
Dave De Breuck <dave.debreuck@twikit.com>
2022-03-03 15:20:31 +0100

    Boyscout CMAKE scripts
    Boyscout directories
    Boyscout precompiled header

--- cmake/directories.cmake
+++ cmake/directories.cmake
@@ -1,13 +1,15 @@
 # -------------------------------
 # Directories       
 # -------------------------------
-SET (SCRIPTS_DIR ${PROJECT_SOURCE_DIR}\\scripts)
-SET (SRC_DIR ${PROJECT_SOURCE_DIR}\\source)
-SET (EXTERNALS_DIR ${PROJECT_SOURCE_DIR}\\extern)
+SET (SCRIPTS_DIR ${CMAKE_SOURCE_DIR}\\scripts)
+SET (SRC_DIR ${CMAKE_SOURCE_DIR}\\source)
+SET (EXTERNALS_DIR ${CMAKE_SOURCE_DIR}\\external)
 SET (MODULES_DIR ${SRC_DIR}\\modules)
-SET (BIN_DIR ${PROJECT_BINARY_DIR}\\bin)
+SET (BIN_DIR ${CMAKE_BINARY_DIR}\\bin)
 
-set(LIB_SDL2 ${EXTERNALS_DIR}/sdl-windows/lib/x64/SDL2.lib)
-set(LIB_SDL2_MAIN ${EXTERNALS_DIR}/sdl-windows/lib/x64/SDL2main.lib)
-
-set(INCLUDE_SDL2 ${EXTERNALS_DIR}/sdl-windows/include)
+IF(${PLATFORM_TARGET} STREQUAL ${WINDOWS})
+    set(LIB_SDL2 ${EXTERNALS_DIR}/sdl-windows/lib/x64/SDL2.lib)
+    set(LIB_SDL2_MAIN ${EXTERNALS_DIR}/sdl-windows/lib/x64/SDL2main.lib)
+    
+    set(INCLUDE_SDL2 ${EXTERNALS_DIR}/sdl-windows/include)
+ENDIF()
