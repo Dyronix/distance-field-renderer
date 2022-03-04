c85c1e333f05f6fe4f39af5c0cb7b4e721716a68
Dave De Breuck <dave.debreuck@twikit.com>
2022-03-04 09:34:39 +0100

    Precompiled headers
    regina precompiled header update
    rex-core precompiled header update

--- source/include/1_foundations/rex_core/rex_core_pch.h
+++ source/include/1_foundations/rex_core/rex_core_pch.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <optional>
+
 #include "types.h"
+#include "defines.h"
+
+#include "rex_debug.h"
--- source/include/3_tools/regina/regina_pch.h
+++ source/include/3_tools/regina/regina_pch.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <optional>
+
+#include "types.h"
+#include "defines.h"
+
+#include "application_arguments.h"
