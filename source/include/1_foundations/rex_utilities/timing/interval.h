0dd62ed58abd8977ea61c429227f5dc297fb5aa7
Dave De Breuck <dave.debreuck@twikit.com>
2022-03-04 15:09:08 +0100

    Timing utilities

---
+++ source/include/1_foundations/rex_utilities/timing/interval.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "timing/tickcount.h"
+
+namespace rex
+{
+    class Interval
+    {
+    public:
+        //-------------------------------------------------------------------------
+        Interval() 
+            : m_initial(getTickCount())
+        {}
+        //-------------------------------------------------------------------------
+        ~Interval() = default;
+
+        //-------------------------------------------------------------------------
+        inline uint64 value() const
+        {
+            return getTickCount() - m_initial;
+        }
+
+    private:
+        uint64 m_initial;
+    };
+
+}
---
+++ source/include/1_foundations/rex_utilities/timing/tickcount.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "types.h"
+
+#include <chrono>
+
+namespace rex
+{
+    //-------------------------------------------------------------------------
+    // simulation of Windows GetTickCount()
+    inline uint64 getTickCount()
+    {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+    }
+
+    //-------------------------------------------------------------------------
+    // Clock built upon Windows GetTickCount()
+    struct TickCountClock
+    {
+        typedef uint64                                   rep;
+        typedef std::milli                               period;
+        typedef std::chrono::duration<rep, period>       duration;
+        typedef std::chrono::time_point<TickCountClock>  time_point;
+
+        static const bool is_steady = true;
+
+        //-------------------------------------------------------------------------
+        static time_point now() noexcept
+        {
+            return time_point(duration(getTickCount()));
+        }
+    };
+}
