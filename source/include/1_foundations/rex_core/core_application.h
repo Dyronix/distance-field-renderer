2437923268309d1c4038324a0c09b70798893092
Dave De Breuck <dave.debreuck@twikit.com>
2022-03-04 15:13:45 +0100

    Core Application
    Initialize SDL
    Create Window
    Create Display Manager
    Start Application Loop

--- source/include/1_foundations/rex_core/core_application.h
+++ source/include/1_foundations/rex_core/core_application.h
@@ -3,22 +3,66 @@
 namespace rex
 {
     class ApplicationArguments;
+    class Window;
+    class DisplayManager;
+
+    struct FrameInfo;
 
     struct ApplicationDescription
     {
+        const char* name;
+
         int32 window_width;
         int32 window_height;
 
         bool fullscreen;
     };
 
-    class CoreApplication
+    struct ApplicationLoop
+    {
+        virtual ~ApplicationLoop() = default;
+
+        virtual void loop() = 0;
+    };
+
+    class CoreApplication : public ApplicationLoop
     {
     public:
         CoreApplication(const ApplicationDescription& description);
         virtual ~CoreApplication();
 
+        bool is_running() const;
+        bool is_marked_for_destruction() const;
+
         int32 run();
+        void quit();
+
+    protected:
+        void mark_for_destroy();
+
+        void initialize();
+        void update();
+        void shutdown();
+
+        virtual void on_initialize() { /**/ }
+        virtual void on_update(const FrameInfo& /*info*/) { /**/ }
+        virtual void on_shutdown() { /**/ }
+
+    private:
+        void loop() final;
+
+        void create_display_manager();
+        void create_window();
+
+        static CoreApplication* s_instance;
+        
+        bool m_is_marked_for_destruction;
+        bool m_is_running;
+
+        ApplicationDescription m_description;
+
+        std::unique_ptr<DisplayManager> m_display_manager;
+        std::unique_ptr<Window> m_window;
     };
 
     // This will be implemented by the CLIENT
