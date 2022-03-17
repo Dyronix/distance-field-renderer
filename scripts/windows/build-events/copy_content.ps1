2fb5da85b7c10a99ec20d8f5ecb1cde3e8d91d82
Dave De Breuck <dave.debreuck@twikit.com>
2022-03-17 21:57:51 +0100

    Post build events
    Copy SDL DLL
    Copy Resources

--- scripts/windows/build-events/copy_content.ps1
+++ scripts/windows/build-events/copy_content.ps1
@@ -5,27 +5,24 @@ $arguments = $args
 
 $cwd=$arguments[0]
 $output_dir=$arguments[1]
-$external_dir=$arguments[2]
+$content_dir=$arguments[2]
 
 if([string]::IsNullOrEmpty($cwd))
 {
 	Write-Error "Empty working directory, pass the working directory for this script as an argument"
 }
 
-Write-Host "Loading windows/build-events/copy_sdl2_dll.ps1" -ForegroundColor Green
+Write-Host "Loading windows/build-events/copy_content.ps1" -ForegroundColor Green
 Write-Host "Current working directory: $cwd"
 
 try
 {
     Push-Location -Path "$output_dir"
-        if (!(Test-Path "SDL2.dll")) 
-        {
-            Copy-Item -Path "$external_dir\sdl-windows\lib\x64\SDL2.dll"
-        }   
+        Copy-Item -Path "$content_dir" -Destination "$output_dir" -Recurse -Force
     Pop-Location
 }
 catch
 {
-    Write-Host "Could not copy item: $external_dir\sdl-windows\lib\x64\SDL2.dll"
+    Write-Host "Could not copy item: $content_dir"
     Write-Host $_
 }
