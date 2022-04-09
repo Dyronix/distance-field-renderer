fb3e91c8b0a87ddfadc5e5c10260ed95cba9ca69
Dave De Breuck <dave.debreuck@twikit.com>
2022-04-09 21:12:14 +0200

    Regina application

--- source/include/3_tools/regina/regina.h
+++ source/include/3_tools/regina/regina.h
@@ -2,6 +2,13 @@
 
 #include "win32_application.h"
 
+namespace rex
+{
+    struct FrameInfo;
+
+    class Layer;
+} // namespace rex
+
 namespace regina
 {
     class Application : public rex::win32::Application
@@ -11,5 +18,11 @@ namespace regina
         ~Application() override;
 
         void on_app_initialize() override;
+        void on_app_update(const rex::FrameInfo& info) override;
+
+    private:
+        rex::Layer* m_deferred_layer;
+        rex::Layer* m_distance_field_layer;
+        rex::Layer* m_shotcut_layer;
     };
 }
