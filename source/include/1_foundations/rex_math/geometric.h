a6d11a96609a3576e82044378d383df1689d42c0
Dave De Breuck <dave.debreuck@twikit.com>
2022-04-09 23:49:07 +0200

    vec2 lerp vec3 lerp vec4 lerp

--- source/include/1_foundations/rex_math/geometric.h
+++ source/include/1_foundations/rex_math/geometric.h
@@ -1,9 +1,48 @@
 #pragma once
 
+#include "algorithms/lerp.h"
+
 namespace rex
 {
     using vector_size_type = int;
     using vector_qualifier = glm::qualifier;
+
+    //-------------------------------------------------------------------------
+    template <typename T, vector_qualifier Q>
+    glm::vec<2, T, Q> lerp(const glm::vec<2, T, Q>& a, const glm::vec<2, T, Q>& b, T t)
+    {
+        glm::vec<2, T, Q> res;
+
+        res.x = lerp(a.x, b.x, t);
+        res.y = lerp(a.y, b.y, t);
+
+        return res;
+    }
+    //-------------------------------------------------------------------------
+    template <typename T, vector_qualifier Q>
+    glm::vec<3, T, Q> lerp(const glm::vec<3, T, Q>& a, const glm::vec<3, T, Q>& b, T t)
+    {
+        glm::vec<3, T, Q> res;
+
+        res.x = lerp(a.x, b.x, t);
+        res.y = lerp(a.y, b.y, t);
+        res.z = lerp(a.z, b.z, t);
+
+        return res;
+    }
+    //-------------------------------------------------------------------------
+    template <typename T, vector_qualifier Q>
+    glm::vec<4, T, Q> lerp(const glm::vec<4, T, Q>& a, const glm::vec<4, T, Q>& b, T t)
+    {
+        glm::vec<4, T, Q> res;
+
+        res.x = lerp(a.x, b.x, t);
+        res.y = lerp(a.y, b.y, t);
+        res.y = lerp(a.z, b.y, t);
+        res.y = lerp(a.w, b.y, t);
+
+        return res;
+    }
 
     //-------------------------------------------------------------------------
     template <vector_size_type L, typename T, vector_qualifier Q>
